2717a9799c171d42f72cc07607a7bc12f7858a66
Egor Popov <ijfgio@yandex.ru>
2024-03-19 17:42:45 +0300

    fix gradle

---
+++ src/test/java/ru/netology/service/CashBackHackServiceTest.java
@@ -0,0 +1,27 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashBackHackServiceTest {
+
+    CashBackService service = new CashBackService();
+
+    @Test
+    public void shouldCorrectAmount() {
+         assertEquals(service.remain(2800), 200);
+
+    }
+
+    @Test
+    public void shouldAmountZero() {
+        assertEquals(service.remain(0), 1000);
+    }
+
+    @Test
+    public void shouldBuyExtraIfTheAmount1000() {
+        assertEquals(service.remain(1000), 0);
+    }
+
+}
